37302a5698b616ad0f32e912b0386bbc9752a9aa
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-02-19 23:45:33 -0300

    Global Styles - Reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { FormikEnvironment } from './pages/FormikEnvironment'
+import { GlobalStyle } from './styles/global'
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<FormikEnvironment />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FormikEnvironment />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
