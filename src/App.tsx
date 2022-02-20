d1ce93f273df8345d0d66bbafdd414ea288d847f
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-02-19 23:51:07 -0300

    added theme design colors and breakpoints

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { FormikEnvironment } from './pages/FormikEnvironment'
 import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './styles/theme'
 
 function App() {
 
   return (
     <>
       <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<FormikEnvironment />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<FormikEnvironment />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   )
 }
