84b14c3cb5b97a02721b8a3aa63b0d6790c3686c
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-02-20 18:55:40 -0300

    Created Component Image

--- src/pages/FormikEnvironment/index.tsx
+++ src/pages/FormikEnvironment/index.tsx
@@ -1,9 +1,11 @@
 import { Formik } from 'formik';
 import { Button } from '../../components/ButtonDefault';
+import { Image } from '../../components/Image';
 
 export function FormikEnvironment() {
   return (
     <>
+      <Image src='https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' />
       <Formik
         // validationSchema={ }
         initialValues={{}}
