1210de645059077187bfd8b2abc2dd5e30693893
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-02-19 23:42:48 -0300

    Created Button default

--- src/pages/FormikEnvironment/index.tsx
+++ src/pages/FormikEnvironment/index.tsx
@@ -1,19 +1,22 @@
 import { Formik } from 'formik';
+import { Button } from '../../components/ButtonDefault';
 
 export function FormikEnvironment() {
   return (
     <>
-    
       <Formik
-        // validationSchema={}
+        // validationSchema={ }
         initialValues={{}}
         onSubmit={(values, { setSubmitting }) => {
           console.log(values)
         }} >
-           {({ handleChange, handleBlur, handleSubmit, values, setFieldValue, errors, touched  }) => (
-             <input type={'text'} onChange={handleChange} onBlur={handleBlur} value={'values'} />
-           )}
+        {({ handleChange, handleBlur, handleSubmit, values, setFieldValue, errors, touched }) => (
+          <>
+            <input type={'text'} onChange={handleChange} onBlur={handleBlur} value={'values'} />
+            <Button>Submit</Button>
+          </>
+        )}
       </Formik>
     </>
-    )
+  )
 }
